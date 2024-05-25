7eae9be52256e597be58e6a33d8b1ef3e4c943f5
michacassola <mich@casso.la>
2024-05-25 14:53:59 +0200

    Update unypkg-cmake-build.sh

--- unypkg-cmake-build.sh
+++ unypkg-cmake-build.sh
@@ -77,7 +77,7 @@ get_include_paths
 
 #unset LD_RUN_PATH
 
-CMAKE_PREFIX_PATH="$(for i in /uny/pkg/*/*; do echo -n "$i"\;; done)"
+CMAKE_PREFIX_PATH="$(for i in /uny/pkg/*/*; do echo -n "$i"\:; done)"
 export CMAKE_PREFIX_PATH
 
 ./bootstrap --prefix=/uny/pkg/"$pkgname"/"$pkgver" \
